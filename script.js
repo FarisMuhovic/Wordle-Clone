33720d49b484bb1e304bd776f33b4a096a1659d9
FarisMuhovic <faris.fare123456@gmail.com>
2022-12-11 13:29:56 +0100

    added graphical letter import and logic

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ const board = document.getElementById("board");
 // 1 row has 5 cols
 // 6 rows max
 for (let i = 0; i < 6; i++) {
-  board.innerHTML += `<div id="rows" class="row">
-    <div class="tile" id="tile">
+  board.innerHTML += `<div class="row">
+    <div class="tile">
     </div>
-    <div class="tile" id="tile">
+    <div class="tile">
     </div>
-    <div class="tile" id="tile">
+    <div class="tile">
     </div>
-    <div class="tile" id="tile">
+    <div class="tile">
     </div>
-    <div class="tile" id="tile">
+    <div class="tile">
     </div>
   </div>`;
 }
@@ -25,56 +25,151 @@ const keyboard = document.querySelectorAll("#keyboard div button");
 let guess = "";
 let arrayofGuesses = [[], [], [], [], [], []];
 let rowcount = 0;
-// keyboard buttons pressed value
-keyboard.forEach(key => {
-  key.addEventListener("click", e => {
-    // make the guess string a 5 letter word
-    guess += e.target.innerText;
-    console.log(guess);
-    //  check if the guess length is 5
-    if (guess.length == 5) {
-      // if the words list contains that guess move on
-      if (WORDS.includes(guess.toLowerCase())) {
-        WORDS.forEach(word => {
-          // find the word that is equal to the guess
-          if (guess.toLowerCase() == word) {
-            // put it in the array as row guess.
-            arrayofGuesses[rowcount].splice(0, 0, guess);
-            console.log(arrayofGuesses);
-            // reset the guess because we dont need it anymore
-            guess = "";
-            // increase the row count so the next word can be put in the nth row line
-            rowcount++;
-            // if the guess is equal to the word we need to guess end the game and declare a win
-            if (word == wordToGuess) {
-              console.log("you guessed it, the word is", wordToGuess);
-            } 
-            // check if that word has letters in the final(given) word
-            for (let i = 0; i < word.length; i++) {
-              if (wordToGuess.includes(word[i])) {
-                // now the word has those letters now check the order
-                // if the order of the word is ok make it green
-                if (wordToGuess[i] == word[i]) {
-                  console.log(word[i], "you guessed it in the right order.");
-                }
-                // if its not in the order make it orange
-                else {
-                  console.log(
-                    word[i],
-                    "The letter is there but not in the right order."
-                  );
-                }
-              }
-            }
-            // no else statement it is ilogical
-          }
-        });
-        // if it doesent contain that word somehow reset the guess
-      } else {
-        console.log("it is not in words list");
-        guess = "";
-      }
-    } // else do nothing
-  });
+let guessCount = 0;
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+window.addEventListener("keyup", e => {
+  appLogic(e, "keyboard");
 });
-// transform the es6 arrow function into normal function for better usage in keyboard typing.
+keyboard.forEach(key => {
+  key.onclick = appLogic;
+});
+function appLogic(e, inputtype) {
+  let boardDivs = board.childNodes[rowcount].childNodes;
+  let filteredBoardDivs = [];
+  for (let i = 0; i < boardDivs.length; i++) {
+    if (i % 2 != 0) {
+      filteredBoardDivs.splice(i, 0, boardDivs[i]);
+    }
+  }
+  console.log(filteredBoardDivs);
+
+  if (inputtype == "keyboard") {
+    let keypressed = e.key.toLowerCase();
+    console.log(keypressed);
+    if (guess.length < 5 && alphabet.includes(keypressed)) {
+      filteredBoardDivs[guessCount].innerText = keypressed.toUpperCase();
+      guess += keypressed;
+      guessCount++;
+    } else if (keypressed == "enter" && guess.length == 5) {
+      console.log("check word");
+      if (WORDS.includes(guess)) {
+        console.log(guess, "the word is in the system");
+        arrayofGuesses[rowcount].splice(0, 0, guess);
+        console.log(arrayofGuesses);
+        for (let i = 0; i < guess.length; i++) {
+          console.log(guess[i]);
+          if (wordToGuess.includes(guess[i])) {
+            if (guess == wordToGuess) {
+              console.log("you guessed it, the word is", wordToGuess);
+            }
+            if (wordToGuess[i] == guess[i]) {
+              console.log(guess[i], "you guessed it in the right order.");
+              filteredBoardDivs[i].style.backgroundColor = "green";
+            }
+            // if its not in the order make it orange
+            else {
+              console.log(
+                guess[i],
+                "The letter is there but not in the right order."
+              );
+              filteredBoardDivs[i].style.backgroundColor = "rgb(219, 143, 0)";
+            }
+          }
+        }
+        rowcount++;
+        guessCount = 0;
+        guess = "";
+      } else {
+        console.log("invalid word");
+      }
+      console.log(rowcount);
+    } else if (
+      keypressed == "backspace" &&
+      guess.length > 0 &&
+      guessCount > 0
+    ) {
+      console.log("del last word");
+      guessCount--;
+      filteredBoardDivs[guessCount].innerText = "";
+      guess = guess.slice(0, -1);
+      console.log(guess);
+    }
+  } else {
+    let keypressed = e.target.innerText.toLowerCase();
+    console.log(keypressed);
+    if (guess.length < 5 && alphabet.includes(keypressed)) {
+      filteredBoardDivs[guessCount].innerText = keypressed.toUpperCase();
+      guess += keypressed;
+      guessCount++;
+    } else if (keypressed == "enter" && guess.length == 5) {
+      console.log("check word");
+      if (WORDS.includes(guess)) {
+        console.log(guess, "the word is in the system");
+        arrayofGuesses[rowcount].splice(0, 0, guess);
+        console.log(arrayofGuesses);
+        for (let i = 0; i < guess.length; i++) {
+          console.log(guess[i]);
+          if (wordToGuess.includes(guess[i])) {
+            if (guess == wordToGuess) {
+              console.log("you guessed it, the word is", wordToGuess);
+            }
+            if (wordToGuess[i] == guess[i]) {
+              console.log(guess[i], "you guessed it in the right order.");
+              filteredBoardDivs[i].style.backgroundColor = "green";
+            }
+            // if its not in the order make it orange
+            else {
+              console.log(
+                guess[i],
+                "The letter is there but not in the right order."
+              );
+              filteredBoardDivs[i].style.backgroundColor = "rgb(219, 143, 0)";
+            }
+          }
+        }
+        rowcount++;
+        guessCount = 0;
+        guess = "";
+      } else {
+        console.log("invalid word");
+      }
+      console.log(rowcount);
+    } else if (keypressed == "del" && guess.length > 0 && guessCount > 0) {
+      console.log("del last word");
+      guessCount--;
+      filteredBoardDivs[guessCount].innerText = "";
+      guess = guess.slice(0, -1);
+      console.log(guess);
+    }
+  }
+  console.log(guess);
+  console.log(guessCount);
+}
