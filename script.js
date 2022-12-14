336c84af5fdf1910e428f7c93141bd1296ee1b2e
FarisMuhovic <faris.fare123456@gmail.com>
2022-12-15 00:03:22 +0100

    added statistics & light mode

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const board = document.getElementById("board");
 const deficitLettersModul = document.getElementById("deficitLetters");
 const notInWordListModul = document.getElementById("notInWordList");
 const statisticsModul = document.getElementById("statistics");
+const winLoseText = document.getElementById('winLoseText')
 // 1 row has 5 cols
 // 6 rows max
 for (let i = 0; i < 6; i++) {
@@ -58,9 +59,41 @@ const alphabet = [
   "z",
 ];
 // statistics
-let played = 0;
-let winrate = 0;
-let streak = 0;
+const winRateUI = document.getElementById("winrate");
+const gamesPlayedUI = document.getElementById("gamesPlayed");
+const streakUI = document.getElementById("streak");
+
+let played = null;
+if (localStorage.getItem("gamesPlayed") != null) {
+  played = localStorage.getItem("gamesPlayed");
+} else {
+  played = 0;
+}
+
+let wins = null;
+if (localStorage.getItem("wins") != null) {
+  wins = localStorage.getItem("wins");
+} else {
+  wins = 0;
+}
+
+let losses = null;
+if (localStorage.getItem("losses") != null) {
+  losses = localStorage.getItem("losses");
+} else {
+  losses = 0;
+}
+
+let winrate = (wins / played) * 100;
+let streak = null;
+if (localStorage.getItem("streak") != null) {
+  streak = localStorage.getItem("streak");
+} else {
+  streak = 0;
+}
+gamesPlayedUI.innerHTML = localStorage.getItem("gamesPlayed");
+streakUI.innerHTML = localStorage.getItem("streak");
+winRateUI.innerHTML = Math.round(winrate, 2);
 window.addEventListener("keyup", e => {
   appLogic(e, "keyboard");
 });
@@ -75,43 +108,57 @@ function appLogic(e, inputtype) {
       filteredBoardDivs.splice(i, 0, boardDivs[i]);
     }
   }
-  console.log(board.childNodes[rowcount]);
   if (inputtype == "keyboard") {
     let keypressed = e.key.toLowerCase();
-    console.log(keypressed);
     if (guess.length < 5 && alphabet.includes(keypressed)) {
       filteredBoardDivs[guessCount].innerText = keypressed.toUpperCase();
       guess += keypressed;
       guessCount++;
     } else if (keypressed == "enter" && guess.length == 5) {
-      console.log("check word");
       if (WORDS.includes(guess)) {
-        console.log(guess, "the word is in the system");
         arrayofGuesses[rowcount].splice(0, 0, guess);
-        console.log(arrayofGuesses);
+        if (arrayofGuesses[5].length == 1 && !wordToGuess.includes(guess)) {
+          played++;
+          streak = 0;
+          losses++;
+          localStorage.setItem("gamesPlayed", played);
+          localStorage.setItem("losses", losses);
+          localStorage.setItem("streak", streak);
+          gamesPlayedUI.innerHTML = localStorage.getItem("gamesPlayed");
+          streakUI.innerHTML = localStorage.getItem("streak");
+          winrate = (wins / played) * 100;
+          winRateUI.innerHTML = Math.round(winrate, 2);
+          setTimeout(() => {
+            statisticsModul.style.display = "grid";
+          }, 3500);
+          winLoseText.innerHTML = `You failed,The word was ${wordToGuess}.`
+        }
         for (let i = 0; i < guess.length; i++) {
           filteredBoardDivs[i].classList.add("tileborder");
-          console.log(guess[i]);
           if (wordToGuess.includes(guess[i])) {
             if (guess == wordToGuess) {
-              console.log("you guessed it, the word is", wordToGuess);
               document.getElementById("keyboard").classList.add("afterwin");
               board.childNodes[rowcount].classList.add("winanimation");
               setTimeout(() => {
                 statisticsModul.style.display = "grid";
               }, 3500);
-              console.log(board.childNodes[rowcount]);
               // disable the board after the win
               rowcount = null;
+              played++;
+              streak++;
+              wins++;
+              localStorage.setItem("gamesPlayed", played);
+              localStorage.setItem("wins", wins);
+              localStorage.setItem("streak", streak);
+              gamesPlayedUI.innerHTML = localStorage.getItem("gamesPlayed");
+              streakUI.innerHTML = localStorage.getItem("streak");
+              winrate = (wins / played) * 100;
+              winRateUI.innerHTML = Math.round(winrate, 2);
+              winLoseText.innerHTML = `You guessed It,The word was ${wordToGuess}.`
             }
             if (wordToGuess[i] == guess[i]) {
-              console.log(guess[i], "you guessed it in the right order.");
               filteredBoardDivs[i].style.backgroundColor = "green";
             } else {
-              console.log(
-                guess[i],
-                "The letter is there but not in the right order."
-              );
               filteredBoardDivs[i].style.backgroundColor = "rgb(219, 143, 0)";
             }
           }
@@ -120,23 +167,19 @@ function appLogic(e, inputtype) {
         guessCount = 0;
         guess = "";
       } else {
-        console.log("invalid word");
         notInWordListModul.style.display = "block";
         setTimeout(() => {
           notInWordListModul.style.display = "none";
         }, 1000);
       }
-      console.log(rowcount);
     } else if (
       keypressed == "backspace" &&
       guess.length > 0 &&
       guessCount > 0
     ) {
-      console.log("del last word");
       guessCount--;
       filteredBoardDivs[guessCount].innerText = "";
       guess = guess.slice(0, -1);
-      console.log(guess);
     } else if (guess.length < 5) {
       deficitLettersModul.style.display = "block";
       setTimeout(() => {
@@ -145,41 +188,51 @@ function appLogic(e, inputtype) {
     }
   } else {
     let keypressed = e.target.innerText.toLowerCase();
-    console.log(keypressed);
     if (guess.length < 5 && alphabet.includes(keypressed)) {
       filteredBoardDivs[guessCount].innerText = keypressed.toUpperCase();
       guess += keypressed;
       guessCount++;
     } else if (keypressed == "enter" && guess.length == 5) {
-      console.log("check word");
       if (WORDS.includes(guess)) {
-        console.log(guess, "the word is in the system");
         arrayofGuesses[rowcount].splice(0, 0, guess);
-        console.log(arrayofGuesses);
+        if (arrayofGuesses[5].length == 1 && !wordToGuess.includes(guess)) {
+          played++;
+          streak = 0;
+          losses++;
+          localStorage.setItem("gamesPlayed", played);
+          localStorage.setItem("losses", losses);
+          localStorage.setItem("streak", streak);
+          gamesPlayedUI.innerHTML = localStorage.getItem("gamesPlayed");
+          streakUI.innerHTML = localStorage.getItem("streak");
+          winrate = (wins / played) * 100;
+          winRateUI.innerHTML = Math.round(winrate, 2);
+          setTimeout(() => {
+            statisticsModul.style.display = "grid";
+          }, 3500);
+          winLoseText.innerHTML = `You failed,The word was ${wordToGuess}.`
+        }
         for (let i = 0; i < guess.length; i++) {
           filteredBoardDivs[i].classList.add("tileborder");
-          console.log(guess[i]);
           if (wordToGuess.includes(guess[i])) {
             if (guess == wordToGuess) {
-              console.log("you guessed it, the word is", wordToGuess);
               document.getElementById("keyboard").classList.add("afterwin");
               board.childNodes[rowcount].classList.add("winanimation");
               setTimeout(() => {
                 statisticsModul.style.display = "grid";
               }, 3500);
-              console.log(board.childNodes[rowcount]);
               // disable the board after the win
               rowcount = null;
-            }
-            if (wordToGuess[i] == guess[i]) {
-              console.log(guess[i], "you guessed it in the right order.");
-              filteredBoardDivs[i].style.backgroundColor = "green";
-            } else {
-              console.log(
-                guess[i],
-                "The letter is there but not in the right order."
-              );
-              filteredBoardDivs[i].style.backgroundColor = "rgb(219, 143, 0)";
+              played++;
+              streak++;
+              wins++;
+              localStorage.setItem("gamesPlayed", played);
+              localStorage.setItem("wins", wins);
+              localStorage.setItem("streak", streak);
+              gamesPlayedUI.innerHTML = localStorage.getItem("gamesPlayed");
+              streakUI.innerHTML = localStorage.getItem("streak");
+              winrate = (wins / played) * 100;
+              winRateUI.innerHTML = Math.round(winrate, 2);
+              winLoseText.innerHTML = `You guessed It,The word was ${wordToGuess}.`
             }
           }
         }
@@ -187,19 +240,15 @@ function appLogic(e, inputtype) {
         guessCount = 0;
         guess = "";
       } else {
-        console.log("invalid word");
         notInWordListModul.style.display = "block";
         setTimeout(() => {
           notInWordListModul.style.display = "none";
         }, 1000);
       }
-      console.log(rowcount);
     } else if (keypressed == "del" && guess.length > 0 && guessCount > 0) {
-      console.log("del last word");
       guessCount--;
       filteredBoardDivs[guessCount].innerText = "";
       guess = guess.slice(0, -1);
-      console.log(guess);
     } else if (guess.length < 5) {
       deficitLettersModul.style.display = "block";
       setTimeout(() => {
@@ -207,10 +256,8 @@ function appLogic(e, inputtype) {
       }, 1000);
     }
   }
-  console.log(guess);
-  console.log(guessCount);
 }
-/// and pop up the statisctics
+
 const exitStatsBtn = document.getElementById("exitStats");
 exitStatsBtn.addEventListener("click", () => {
   statisticsModul.style.display = "none";
@@ -220,6 +267,66 @@ playAgain.addEventListener("click", () => {
   location.reload();
 });
 
-// player loses
-// make statistics work
-// local storage
+const label = document.getElementById("labels");
+
+label.addEventListener("change", e => {
+  localStorage.setItem("mode", e.target.checked);
+  themeMode(e.target.checked);
+});
+
+themeMode(localStorage.getItem("mode"));
+if (localStorage.getItem("mode") == "false") {
+  document.getElementById("checky").checked = false;
+} else {
+  document.getElementById("checky").checked = true;
+}
+function themeMode(boolVal) {
+  if (boolVal == false || boolVal == "false") {
+    // dark mode
+    document.documentElement.style.setProperty("--bg-color", "rgb(29, 29, 29");
+    document.documentElement.style.setProperty(
+      "--tiles-color",
+      "rgb(49, 49, 49)"
+    );
+    document.documentElement.style.setProperty(
+      "--font-color",
+      "rgb(221, 221, 221)"
+    );
+    document.documentElement.style.setProperty(
+      "--nav-border",
+      "rgb(102, 102, 102)"
+    );
+    document.documentElement.style.setProperty(
+      "--tile-hover-color",
+      "rgb(134, 133, 133)"
+    );
+    document.documentElement.style.setProperty(
+      "--stats-bg-color",
+      "rgba(0, 0, 0, 0.877)"
+    );
+  } else if (boolVal == true || boolVal == "true") {
+    //light mode
+    document.documentElement.style.setProperty(
+      "--bg-color",
+      "rgb(233, 233, 233)"
+    );
+    document.documentElement.style.setProperty(
+      "--tiles-color",
+      "rgb(233, 233, 233)"
+    );
+    document.documentElement.style.setProperty("--font-color", "rgb(0, 0, 0)");
+    document.documentElement.style.setProperty(
+      "--nav-border",
+      "rgb(22, 22, 22)"
+    );
+    document.documentElement.style.setProperty(
+      "--stats-bg-color",
+      "rgba(255, 255, 255, 0.377)"
+    );
+  }
+}
+
+const navChart = document.getElementById("statsClick");
+navChart.addEventListener("click", () => {
+  statisticsModul.style.display = "grid";
+});
